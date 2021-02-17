3410b5be602197e625f11cac819187f11e485c60
farshad vahidifard <farshad.vahidifard@studenti.unimi.it>
2021-02-17 18:18:08 +0100

    RED: test to check route for user

--- tests/Feature/MyTest/multiAuthenticationTest.php
+++ tests/Feature/MyTest/multiAuthenticationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\MyTest;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -13,5 +14,19 @@ class multiAuthenticationTest extends TestCase
     public function only_signed_in_staff_have_access_to_dashboard()
     {
         $this->get('/dashboard')->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function after_check_staff_route_them_to_the_correct_dashboard()
+    {
+        $this->withoutExceptionHandling();
+
+        //these tre line are setting
+        $role = Role::factory()->create(['name' => 'user']);
+        $user = User::factory()->create();
+        $user->attachRole($role);
+
+        $this->assertTrue($user->hasRole('user'));
+        $this->actingAs($user)->get('/dashboard')->assertRedirect('user.dashboard');
     }
 }
