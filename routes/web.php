68e5d7eac09d02d46a55f427c6dadf9dbdf0ef56
farshad vahidifard <farshad.vahidifard@studenti.unimi.it>
2021-02-17 13:11:12 +0100

    GREEN: route&view home added

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::view('/home', 'home');
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
